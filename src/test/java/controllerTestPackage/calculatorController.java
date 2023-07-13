4e9f3f553583783ca83d51e8bec2d599bb26414c
ankeeta431997 <ankeeta431997@gmail.com>
2023-07-13 15:34:54 +0530

    Calculator TestCase

--- src/test/java/controllerTestPackage/calculatorController.java
+++ src/test/java/controllerTestPackage/calculatorController.java
@@ -1,6 +1,10 @@
 package controllerTestPackage;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.mockito.Mock;
 import com.unoveo.securityjwt.calculator.calculatorService;
 import com.google.gson.Gson;
@@ -12,11 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -36,13 +36,44 @@ public class calculatorController {
         calculator = new calculatorService();
         System.out.println("BEFORE EACH METHOD CALL");
     }
+//
+//    @Test
+//    void doPost_withValidRequest_returnsCorrectResult() throws ServletException, IOException {
+//        // Arrange
+//        when(request.getReader()).thenReturn(new BufferedReader(new StringReader("[{\"type\":\"num\",\"value\":\"5\"},{\"type\":\"op\",\"value\":\"+\"},{\"type\":\"num\",\"value\":\"7\"}]")));
+//       // when(request.getReader()).thenReturn(new BufferedReader(new StringReader("[{\"type\":\"num\",\"value\":\"10\"},{\"type\":\"op\",\"value\":\"-\"},{\"type\":\"num\",\"value\":\"5\"}]")));
+//        when(request.isUserInRole("ROLE_ADMIN")).thenReturn(true);
+//
+//        StringWriter stringWriter = new StringWriter();
+//        PrintWriter writer = new PrintWriter(stringWriter);
+//        when(response.getWriter()).thenReturn(writer);
+//
+//        // Act
+//        calculator.doPost(request, response);
+//
+//        // Assert
+//        verify(response).setContentType("application/json");
+//        writer.flush();
+//        ExpressionResponse expressionResponse = new Gson().fromJson(stringWriter.toString(), ExpressionResponse.class);
+//        System.out.println("result after test "+expressionResponse);
+//        assertNotNull(expressionResponse);
+//        assertEquals(12, expressionResponse.getResult());
+//       // assertEquals(5, expressionResponse.getResult());
+//      //  assertEquals(HttpServletResponse.SC_OK, response.getStatus());
+//    }
+@Test
+void runTestsFromExcel() throws ServletException, IOException {
+    Workbook workbook = WorkbookFactory.create(new File("src/test/testData/test.xls"));
+    Sheet sheet = workbook.getSheetAt(0); // Assuming the data is in the first sheet
 
-    @Test
+    for (int rowNum = 1; rowNum <= sheet.getLastRowNum(); rowNum++) {
+        Row row = sheet.getRow(rowNum);
+        int id = (int) row.getCell(0).getNumericCellValue();
+        String expressionRequest = row.getCell(1).getStringCellValue();
+        double expected = row.getCell(2).getNumericCellValue();
 
-    void doPost_withValidRequest_returnsCorrectResult() throws ServletException, IOException {
         // Arrange
-        when(request.getReader()).thenReturn(new BufferedReader(new StringReader("[{\"type\":\"num\",\"value\":\"5\"},{\"type\":\"op\",\"value\":\"+\"},{\"type\":\"num\",\"value\":\"7\"}]")));
-       // when(request.getReader()).thenReturn(new BufferedReader(new StringReader("[{\"type\":\"num\",\"value\":\"10\"},{\"type\":\"op\",\"value\":\"-\"},{\"type\":\"num\",\"value\":\"5\"}]")));
+        when(request.getReader()).thenReturn(new BufferedReader(new StringReader(expressionRequest)));
         when(request.isUserInRole("ROLE_ADMIN")).thenReturn(true);
 
         StringWriter stringWriter = new StringWriter();
@@ -53,17 +84,18 @@ public class calculatorController {
         calculator.doPost(request, response);
 
         // Assert
-        verify(response).setContentType("application/json");
+     //   verify(response).setContentType("application/json");
         writer.flush();
         ExpressionResponse expressionResponse = new Gson().fromJson(stringWriter.toString(), ExpressionResponse.class);
-        System.out.println("result after test "+expressionResponse);
+        //System.out.println("Result after test " + expressionResponse);
         assertNotNull(expressionResponse);
-        assertEquals(12, expressionResponse.getResult());
-       // assertEquals(5, expressionResponse.getResult());
-      //  assertEquals(HttpServletResponse.SC_OK, response.getStatus());
+        System.out.println("expected result :"+expected);
+        System.out.println("Actual Result :"+expressionResponse.getResult());
+        assertEquals(expected, expressionResponse.getResult());
     }
+}
     @Test
-    void doPost_withInvalidUserRole_returnsForbiddenError() throws ServletException, IOException {
+    void doPost_withInvalidUserRole_returnsForbiddenError () throws ServletException, IOException {
         // Arrange
         when(request.isUserInRole("ROLE_ADMIN")).thenReturn(false);
 
